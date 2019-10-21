aed6413425f46d58c4f6be645576f6f70f80cfbd
fesasabil <kijee212@gmail.com>
2019-10-21 20:25:20 +0700

    add queue with mail sederhana

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Queue send email</title>
+</head>
+<body>
+    <center>
+        <h2>My Email</h2>
+    </center>
+
+    <p>Hi,Sir</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptatibus quisquam nesciunt veniam eum repudiandae praesentium, sequi maxime? A recusandae cumque earum eos hic, illum ut molestiae quia suscipit velit?</p>
+
+    <strong>Thank you Sir</strong>
+</body>
+</html>
